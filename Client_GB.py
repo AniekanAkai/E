396eada1395d1a7aeb97035da6ac1c942d7c4952
kh-nguyen-le <johnle@cmail.carleton.ca>
2013-10-01 14:32:33 -0400

    Update Client_GB.py

--- Client_GB.py
+++ Client_GB.py
@@ -8,16 +8,15 @@
 # GP8 in J2 to B7 in J3
 # GP7 in J2 to B8 in J3
 # jumpers on U4-out-B5 ... U4-out-B8
-# jumpers on U3-in-B1 ... U3-in-B3
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)                  # initialise RPi.GPIO
-for i in range(23,26):                  # set up ports 23-25 
-    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # as inputs pull-ups high
-for i in range(7,11):					# set up ports 7-10
-	GPIO.setup(i, GPIO.OUT)				# as LED outputs
-	
+GPIO.setmode(GPIO.BCM) # initialise RPi.GPIO
+for i in range(23,26): # set up ports 23-25
+	GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP) # as inputs pull-ups high
+for i in range(7,11):	# set up ports 7-10
+	GPIO.setup(i, GPIO.OUT)	# as LED outputs
+
 # port aliases
 but_tog = 25
 but_cyc = 24
@@ -38,17 +37,18 @@ GPIO.add_event_detect(but_snd, GPIO.FALLING)
 try:
 	while True:
 		led_status = [GPIO.input(led1), GPIO.input(led2), GPIO.input(led3), GPIO.input(led4)] #update status
-		if GPIO.event_detected(but_snd)
-			print "Sent." #put UDP send led_status here
-		elif GPIO.event_detected(but_cyc)
-			if led_curr == led4 
+		if GPIO.event_detected(but_snd):
+			print led_status #put UDP send led_status here
+		elif GPIO.event_detected(but_cyc):
+			if led_curr == led4:
 				led_curr = led1
 			else:
 				led_curr -= 1
-		elif GPIO.event_detected(but_tog)
+		elif GPIO.event_detected(but_tog):
 			GPIO.output(led_curr, not GPIO.input(led_curr))
-			
+
 except KeyboardInterrupt:
 	GPIO.cleanup()
 GPIO.cleanup()
+
 
