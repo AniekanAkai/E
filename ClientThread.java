5a2e4907de9fd45c2cb0b9b3385977f4c6550ab1
kh-nguyen-le <johnle@cmail.carleton.ca>
2013-10-01 23:00:16 -0400

    Update ClientThread.java

--- ClientThread.java
+++ ClientThread.java
@@ -4,42 +4,46 @@ import java.io.*;
 
 public class ClientThread extends Thread{
 
-    private Socket clientSocket = null;
-	String configuration;
-	
-    ClientThread(Socket socket){
-	super("ClientThread");
-	this.clientSocket =  socket;
-    }
+	private Socket clientSocket = null;
+	String configuration = null;
 
-    
-    public void run(){
-        System.out.println("Connected to "+ clientSocket.toString());
+	ClientThread(Socket socket){
+		super("ClientThread");
+		this.clientSocket = socket;
+	    }
+
+	    
+	public void run(){
+		System.out.println("Connected to "+ clientSocket.toString());
 		try{
-			  PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		while ((configuration = in.readLine()) != null) {
+			System.out.println(configuration);
+			out.println("WIN!!!");	
+			break;
+			
+		}
+			
+		out.close();
+		in.close();
+		
+		clientSocket.close();
 
-			  while ((configuration = in.readLine()) != null) {
-				System.out.println(configuration);
-				
-			 }
-			  out.close();
-			  in.close();
-			  clientSocket.close();
 		}catch(IOException e){
 			e.printStackTrace();
 			System.exit(1);
-		 
+
 		}
-    }
+       }
 
 	String getIP(){
 		return clientSocket.getInetAddress().toString();
 	}
-	
-	
+
+
 	String getConfig(){
 		return configuration;
 	}
-	
+
 }
