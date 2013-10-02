4937db6898b3952965eb71a211b8c428554a8776
kh-nguyen-le <johnle@cmail.carleton.ca>
2013-10-01 23:57:02 -0400

    Update Client_GB.py

--- Client_GB.py
+++ Client_GB.py
@@ -14,17 +14,17 @@
 
 
 import RPi.GPIO as GPIO
-import socket
+import socket, select,sys, string
 from time import sleep
 
 GPIO.setmode(GPIO.BCM) # initialise RPi.GPIO
 GPIO.setwarnings(False)
 for i in range(23,26): # set up ports 23-25
-	GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP) # as inputs pull-ups high
-for i in range(7,12):	# set up ports 8-11
-	GPIO.setup(i, GPIO.OUT)	# as LED outputs
-for i in range(17,19):	# set up ports 0-1
-	GPIO.setup(i, GPIO.OUT, initial=GPIO.HIGH)	# as LED indicators
+        GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP) # as inputs pull-ups high
+for i in range(7,12):   # set up ports 8-11
+        GPIO.setup(i, GPIO.OUT) # as LED outputs
+for i in range(17,19):  # set up ports 0-1
+        GPIO.setup(i, GPIO.OUT, initial=GPIO.HIGH)      # as LED indicators
 
 
 # port aliases
@@ -42,56 +42,56 @@ led_curr = led1
 r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 r.connect(("10.0.0.1",4444))
 
+
 def cyc_callback(but_cyc): #cycles to next led when cycle button is pressed
-	global led_curr, led1, led4
+        global led_curr, led1, led4
 
-	if led_curr == led4:
-		led_curr = led1
-	else:
-		led_curr -= 1
-	
+        if led_curr == led4:
+                led_curr = led1
+        else:
+                led_curr -= 1
+
 def tog_callback(but_tog): #toggles current led when toggle button is pressed
-	global led_curr, led1, led4
+        global led_curr, led1, led4
 
-	GPIO.output(led_curr, not GPIO.input(led_curr))
+        GPIO.output(led_curr, not GPIO.input(led_curr))
 
 def snd_callback(but_snd): #sends led status to server
-	global led_curr, led1, led2, led3, led4
+        global led_curr, led1, led2, led3, led4
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(("10.0.0.1", 4444))
-	led_status = [GPIO.input(led1), GPIO.input(led2), GPIO.input(led3), GPIO.input(led4)] #update status
-	led_str =''.join(map(str,led_status))	
-	s.send(led_str)
-	print led_status
-	s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("10.0.0.1", 4444))
+        led_status = [GPIO.input(led1), GPIO.input(led2), GPIO.input(led3), GPIO.input(led4)] #update s$
+        led_str =''.join(map(str,led_status))
+        s.sendall(led_str)
+        print led_status
+        s.close()
 
-def flash(): #blinks end leds 3 times on correct guess
-	for i in (0,6):
-		sleep(0.25)
-		for num in ledflash:
-			GPIO.output(num, not GPIO.input(num))
-	for num in ledflash:
-		GPIO.output(num, 1)
-		
-	
+def flash(): #blinks end leds 10 times on correct guess
+        for i in (0,20):
+                sleep(0.25)
+                for num in ledflash:
+                        GPIO.output(num, not GPIO.input(num))
+        for num in ledflash:
+                GPIO.output(num, 1)
+        else:
+                led_curr -= 1
+
 #detect button presses
 GPIO.add_event_detect(but_tog, GPIO.FALLING, callback=tog_callback, bouncetime=200)
 GPIO.add_event_detect(but_cyc, GPIO.FALLING, callback=cyc_callback, bouncetime=200)
 GPIO.add_event_detect(but_snd, GPIO.FALLING, callback=snd_callback, bouncetime=200)
 
 flash()
-
 try:
-	while True:
-		sleep(0.11)
-		if (r.recv(1024)=="win"):
-			flash()
-		
-	
+        while True:
+
+                sleep(0.11)
+                if (r.recv(1024)=="win"):
+                        flash()
+
 except KeyboardInterrupt:
-	GPIO.cleanup()
+        GPIO.cleanup()
+        r.close()
 GPIO.cleanup()
-
-
-	
+r.close()
